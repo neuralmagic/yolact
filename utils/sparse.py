054fb33ff33edac1630c147cfd8f18927abe5b8a
Rahul Tuli <rahul@neuralmagic.com>
2021-11-12 16:41:00 -0500

    Add: Train Script support for Yolact - Adds recipe and ckpt arguments - Supports SparseZoo Stubs - Supports resuming training from a previous SparseML checkpoint - The checkpoint can be Quantized or Pruned - Supports overriding checkpoint epoch - Adds a export member variable to Yolact script for supporting export flows - Adds support for weights and biases - Adds Saving logic before mAp Calculation - Turns off JIT

---
+++ utils/sparse.py
@@ -0,0 +1,95 @@
+import math
+
+import torch.nn as nn
+from sparseml.pytorch.optim import ScheduledModifierManager
+from sparseml.pytorch.utils import SparsificationGroupLogger
+
+
+def is_parallel(model):
+    return type(model) in (
+    nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
+
+
+class SparseMLWrapper(object):
+    def __init__(self, model, recipe):
+        self.enabled = bool(recipe)
+        self.model = model.module if is_parallel(model) else model
+        self.recipe = recipe
+        self.manager = ScheduledModifierManager.from_yaml(
+            recipe
+            ) if self.enabled else None
+        self.logger = None
+
+    def state_dict(self):
+        return {
+            'recipe': str(self.manager) if self.enabled else None,
+        }
+
+    def apply(self):
+        if self.enabled:
+            self.manager.apply(self.model)
+
+    def initialize(self, start_epoch):
+        if self.enabled:
+            self.manager.initialize(self.model, start_epoch)
+
+    def initialize_loggers(self, logger, tb_writer, wandb_logger, rank):
+        self.logger = logger
+
+        if self.enabled and rank in [-1, 0]:
+            self.manager.initialize_loggers(
+                [
+                    SparsificationGroupLogger(
+                        tensorboard=tb_writer,
+                        wandb_={'project': 'yolact'} if wandb_logger else None
+
+                    ),
+
+                ]
+            )
+
+    def modify(self, scaler, optimizer, model, dataloader):
+        if self.enabled:
+            return self.manager.modify(
+                model,
+                optimizer,
+                steps_per_epoch=len(dataloader),
+                wrap_optim=scaler
+            )
+
+        return scaler
+
+    def check_lr_override(self, scheduler):
+        # Override lr scheduler if recipe makes any LR updates
+        if self.should_override_scheduler():
+            self.logger.info(
+                'Disabling LR scheduler, managing LR using SparseML recipe'
+                )
+            scheduler = None
+
+        return scheduler
+
+    def should_override_scheduler(self):
+        return self.enabled and self.manager.learning_rate_modifiers
+
+    def check_epoch_override(self, epochs):
+        # Override num epochs if recipe explicitly modifies epoch range
+        if self.enabled and self.manager.epoch_modifiers and \
+                self.manager.max_epochs:
+            epochs = self.manager.max_epochs or epochs  # override num_epochs
+            self.logger.info(
+                f'Overriding number of epochs from SparseML manager to {epochs}'
+                )
+
+        return epochs
+
+    def qat_active(self, epoch):
+        if self.enabled and self.manager.quantization_modifiers:
+            qat_start = min(
+                [mod.start_epoch for mod in self.manager.quantization_modifiers]
+                )
+
+            return qat_start < epoch + 1
+
+        return False
+
